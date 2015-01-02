2c204e80dd7f55693cebb2d805ff3fef7d79f55c
julia-smerechynska <julie@Julies-MacBook-Pro.local>
2015-01-01 21:45:11 -0800

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * This is my first HelloWorld program 
+ * Created by julie on 1/1/15.
+ */
+public class HelloWorld {
+    
+    public static void main (String args[]) {
+        System.out.println ("Hello World!");
+    } // main ends here
+    
+} // HelloWorld ends here
