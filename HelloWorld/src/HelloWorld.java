e48e31def6527ea49bce007e0af2095e9537450b
julia-smerechynska <julie@Julies-MacBook-Pro.local>
2015-01-01 23:18:34 -0800

    fix format

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,7 +5,7 @@
 public class HelloWorld {
     
     public static void main (String args[]) {
-        System.out.println ("Hello World!");
+        System.out.println("Hello World!");
     } // main ends here
     
 } // HelloWorld ends here
